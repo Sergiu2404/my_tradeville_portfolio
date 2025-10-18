619ccb343643927331a3639159eac2729158f74f
Sergiu2404 <sergiu_goian@yahoo.com>
2025-10-18 21:44:48 +0300

    feature: login + extract portfolio

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 # pipeline to extract data from tradeville API and load to postgres db
+import asyncio
+import dotenv
+
 from websocket_tradeville import TradevilleAPIConnection
 
+TRADEVILLE_PORT = "443"
+TRADEVILLE_URI = "wss://api.tradeville.ro"
+
+dotenv.load_dotenv()
+
 tradeville_api_connection = TradevilleAPIConnection()
-tradeville_api_connection.access_api()
+asyncio.run(tradeville_api_connection.portfolio())
--- websocket_tradeville.py
+++ websocket_tradeville.py
@@ -1,23 +1,49 @@
-import asyncio
+import json
 import websockets
-from websockets.typing import Subprotocol
+import dotenv
 
 TRADEVILLE_PORT = "443"
 TRADEVILLE_URI = "wss://api.tradeville.ro"
 
+dotenv.load_dotenv()
+
+
 class TradevilleAPIConnection:
+    '''
+    For more details on the websocket connection to tradeville, access api docs below:
+    https://portal.tradeville.ro/diverse/api/APIdocs.htm#pfolosire
+    '''
     __tradeville_login_header = {
         "cmd": "login",
         "prm": {
-            "coduser": "coduser",
-
+            "coduser": dotenv.dotenv_values(".env")["CODUSER"], #"!DemoAPITDV"
+            "parola": dotenv.dotenv_values(".env")["PASSWORD"], #"DemoAPITDV"
+            "demo": False
         }
     }
-    async def access_api(self):
+    __tradeville_portfolio_header = {
+        "cmd": "Portfolio",
+        "prm": {
+            "data": "null"
+        }
+    }
+
+    def __to_json(self, object):
+        return json.dumps(object)
+
+    async def __login(self, websocket):
+        serialized_json = self.__to_json(self.__tradeville_login_header)
+        await websocket.send(serialized_json)
+        response = await websocket.recv()
+        print(response)
+
+    async def portfolio(self):
         async with websockets.connect(
             uri = f"{TRADEVILLE_URI}:{TRADEVILLE_PORT}", subprotocols=["apitv"]
         ) as websocket:
-            await websocket.send("req")
+            await self.__login(websocket)
+            # request portfolio command
+            serialized_json = self.__to_json(self.__tradeville_portfolio_header)
+            await websocket.send(serialized_json)
             response = await websocket.recv()
             print(response)
-
