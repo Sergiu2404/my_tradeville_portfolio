b51f6aedb9ad6aa12cc57b03aeb61d5c2322d573
Sergiu2404 <sergiu_goian@yahoo.com>
2025-10-21 20:14:40 +0300

    feature: symbol dividends history

--- src/data_sources/yahoofinance_api.py
+++ src/data_sources/yahoofinance_api.py
@@ -3,9 +3,20 @@ import yfinance
 class YahooFinanceAPI:
     def get_symbol_history_data(self, symbol, interval, date_start, date_end):
         data = yfinance.download(tickers=symbol, interval=interval, start=date_start, end=date_end)
-        print(data)
+        return data
+
     def get_symbol_industry_and_sector(self, symbol):
         ticker_data = yfinance.Ticker(symbol)
         info = ticker_data.info
-
         return (info["industry"], info["sector"])
+
+    def get_symbol_dividends_history(self, symbol):
+        '''
+        Get dividends history for symbol
+        :param symbol: Stock symbol
+        :return: Series containing dividends history for given symbol
+        '''
+        symbol_data = yfinance.Ticker(symbol)
+        dividends_data = symbol_data.dividends
+        return dividends_data
+
--- src/main.py
+++ src/main.py
@@ -11,42 +11,43 @@ yahoofinance_api = YahooFinanceAPI()
 (industry, sector) = yahoofinance_api.get_symbol_industry_and_sector("SNP.RO")
 print(industry + "\n" + sector)
 #yahoofinance_api.get_symbol_history_data("INTL", "1mo", "2025-07-01", "2025-10-01")
+print(yahoofinance_api.get_symbol_dividends_history("SNP.RO"))
 
-tradeville_api_connection = TradevilleAPI()
-tradeville_data_processing = TradevilleDataProcessing()
-
-response = asyncio.run(tradeville_api_connection.get_portfolio())
-portfolio_df = tradeville_data_processing.to_dataframe(response["data"])
-print(portfolio_df)
-
-response = asyncio.run(tradeville_api_connection.search_symbol("electric"))
-search_symbol_df = tradeville_data_processing.to_dataframe(response["data"])
-print(search_symbol_df)
-
-response = asyncio.run(tradeville_api_connection.get_symbol_data("BRD"))
-symbol_data = tradeville_data_processing.to_dataframe(response["data"])
-print(symbol_data)
-
-response = asyncio.run(tradeville_api_connection.get_symbol_orders("BRD"))
-symbol_orders = tradeville_data_processing.to_dataframe(response["data"])
-print(symbol_orders)
-
-response = asyncio.run(tradeville_api_connection.get_symbol_trades("BRD", "1oct25", "10oct25"))
-symbol_trades = tradeville_data_processing.to_dataframe(response["data"])
-print(symbol_trades)
-
-response = asyncio.run(tradeville_api_connection.get_account_activity("1jan25", "10oct25"))
-print(response)
-account_activity = tradeville_data_processing.to_dataframe(response["data"])
-print(account_activity)
-
-response = asyncio.run(tradeville_api_connection.get_bnr_exchange_rate("EUR", "1apr25", "10apr25"))
-exchange_rate = tradeville_data_processing.to_dataframe(response["data"])
-print(exchange_rate)
-
-response = asyncio.run(tradeville_api_connection.get_symbol_daily_values("SNP", "1oct25", "15oct25"))
-daily_values = tradeville_data_processing.to_dataframe(response["data"])
-print(daily_values)
+# tradeville_api_connection = TradevilleAPI()
+# tradeville_data_processing = TradevilleDataProcessing()
+#
+# response = asyncio.run(tradeville_api_connection.get_portfolio())
+# portfolio_df = tradeville_data_processing.to_dataframe(response["data"])
+# print(portfolio_df)
+#
+# response = asyncio.run(tradeville_api_connection.search_symbol("electric"))
+# search_symbol_df = tradeville_data_processing.to_dataframe(response["data"])
+# print(search_symbol_df)
+#
+# response = asyncio.run(tradeville_api_connection.get_symbol_data("BRD"))
+# symbol_data = tradeville_data_processing.to_dataframe(response["data"])
+# print(symbol_data)
+#
+# response = asyncio.run(tradeville_api_connection.get_symbol_orders("BRD"))
+# symbol_orders = tradeville_data_processing.to_dataframe(response["data"])
+# print(symbol_orders)
+#
+# response = asyncio.run(tradeville_api_connection.get_symbol_trades("BRD", "1oct25", "10oct25"))
+# symbol_trades = tradeville_data_processing.to_dataframe(response["data"])
+# print(symbol_trades)
+#
+# response = asyncio.run(tradeville_api_connection.get_account_activity("1jan25", "10oct25"))
+# print(response)
+# account_activity = tradeville_data_processing.to_dataframe(response["data"])
+# print(account_activity)
+#
+# response = asyncio.run(tradeville_api_connection.get_bnr_exchange_rate("EUR", "1apr25", "10apr25"))
+# exchange_rate = tradeville_data_processing.to_dataframe(response["data"])
+# print(exchange_rate)
+#
+# response = asyncio.run(tradeville_api_connection.get_symbol_daily_values("SNP", "1oct25", "15oct25"))
+# daily_values = tradeville_data_processing.to_dataframe(response["data"])
+# print(daily_values)
 
 # response = asyncio.run(tradeville_api_connection.get_symbol_market_depth("BRD", 10))
 # print(response)
